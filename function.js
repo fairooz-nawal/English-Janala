5c8b12bea3772c1719b1f9f914aeb09cbf30259c
Syeda Fairooz Nawal <syedafairooznawal@gmail.com>
2025-03-17 21:49:13 +0600

    Adding high lights in the lesson button, adding spinner

--- function.js
+++ function.js
@@ -10,12 +10,12 @@ document.addEventListener('DOMContentLoaded', function () {
     const footerSection = document.querySelector('footer');
     const logoutButton = document.querySelector('a[href="#logout"]');
 
-    
+
     navBar.style.display = 'none';
     learnSection.style.display = 'none';
     faqSection.style.display = 'none';
 
-    
+
     const loginForm = document.querySelector('.hero-content');
     const nameInput = loginForm.querySelector('input[type="text"]');
     const passwordInput = loginForm.querySelector('input[type="password"]');
@@ -37,28 +37,28 @@ document.addEventListener('DOMContentLoaded', function () {
             return;
         }
 
-        
+
         alert('Login successful!');
         bannerSection.style.display = 'none';
-        navBar.style.display = 'flex'; 
-        learnSection.style.display = 'block'; 
-        faqSection.style.display = 'block'; 
+        navBar.style.display = 'flex';
+        learnSection.style.display = 'block';
+        faqSection.style.display = 'block';
     });
 
-    
+
     logoutButton.addEventListener('click', function (event) {
         event.preventDefault();
 
-    
+
         navBar.style.display = 'none';
         learnSection.style.display = 'none';
         faqSection.style.display = 'none';
 
-        
+
         bannerSection.style.display = 'block';
     });
 
-    
+
     const learnButton = document.getElementById('learnmore');
     learnButton.addEventListener('click', function (event) {
         event.preventDefault();
@@ -68,7 +68,7 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 
-    
+
     const faqbtn = document.getElementById('faqbtn');
     faqbtn.addEventListener('click', function (event) {
         event.preventDefault();
@@ -84,11 +84,15 @@ document.addEventListener('DOMContentLoaded', function () {
             if (data.status) {
                 data.data.forEach(level => {
                     const lessonButton = document.createElement('button');
-                    lessonButton.textContent = `Lesson - ${level.level_no}`;
-                    lessonButton.className = 'bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition duration-300';
+                    lessonButton.innerHTML = `
+                    <button id = "${level.level_no}" class="btn border-2 border-blue-700 bg-white hover:bg-blue-400 text-blue-700">
+                    <img src="./assets/fa-book-open.png" alt="">
+                    Lesson- ${level.level_no}
+                    </button>`
+
                     lessonButton.addEventListener('click', () => {
-                        highlightActiveButton(lessonButton);
                         loadVocab(level.level_no);
+                        highlightActiveButton();
                     });
                     lessonsContainer.appendChild(lessonButton);
                 });
@@ -96,35 +100,43 @@ document.addEventListener('DOMContentLoaded', function () {
         })
         .catch(error => console.error('Error fetching levels:', error));
 
-    
-    function highlightActiveButton(activeButton) {
-        const allButtons = lessonsContainer.querySelectorAll('button');
-        allButtons.forEach(button => {
-            button.classList.remove('bg-blue-900');
-            button.classList.add('bg-blue-500');
-        });
 
-        activeButton.classList.remove('bg-blue-500');
-        activeButton.classList.add('bg-blue-900');
+    function highlightActiveButton() {
+        const activeButton = document.getElementsByClassName('active');
+        console.log(activeButton);
+        for (const button of activeButton) {
+            button.classList.remove('active');
+        };
+
     }
 
     function loadVocab(levelNo) {
         vocabCardsContainer.innerHTML = '';
+        vocabCardsContainer.innerHTML = '';
+        const spinner = document.createElement('div');
+        spinner.classList.add('w-[100px]', 'border-3', 'border-blue-600');
+        spinner.className = 'spinner';
+        spinner.innerHTML = '<i class="fa-solid fa-spinner fa-spin text-5xl text-center text-blue-700"></i>';
+        vocabCardsContainer.appendChild(spinner);
         fetch(`https://openapi.programming-hero.com/api/level/${levelNo}`)
             .then(response => response.json())
             .then(data => {
                 if (data.status) {
+                    vocabCardsContainer.removeChild(spinner);
+                    const button = document.getElementById(levelNo);
+                    button.classList.add('active');
                     if (data.data.length === 0) {
                         vocabCardsContainer.innerHTML = '';
                         lessonWord1.innerHTML = `
                             <div class="space-4 text-center py-10">
-                                <i class="fa-solid fa-exclamation"></i>
+                                <img src="./assets/alert-error.png" alt="" class="mx-auto">
                                 <p class="py-6 text-gray-500">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
                                 <h1 class="text-3xl font-bold">নেক্সট Lesson এ যান </h1>  
                             </div>
                         `;
                         lessonWord.innerHTML = '';
                     } else {
+
                         lessonWord1.innerHTML = '';
                         lessonWord.innerHTML = '';
 
@@ -132,19 +144,19 @@ document.addEventListener('DOMContentLoaded', function () {
                             const card = document.createElement('div');
                             card.className = 'bg-white p-6 rounded-lg shadow-lg transition-shadow duration-300';
                             card.innerHTML = `
-                                <h3 class="text-xl font-semibold text-gray-800">${word?.word|| 'No Data'}</h3>
+                                <h3 class="text-xl font-semibold text-gray-800">${word?.word || 'অর্থ নেয়'}</h3>
                                 <p class="text-gray-600 mt-2"><span class="font-medium">Meaning/Pronunciation</p>
-                                <h3 class="text-xl font-semibold text-gray-800">${word?.meaning|| 'No Data'}/${word?.pronunciation|| 'No Data'}</h3>
+                                <h3 class="text-xl font-semibold text-gray-800">${word?.meaning || 'অর্থ নেয়'}/${word?.pronunciation || 'অর্থ নেয়'}</h3>
                                 <div class="flex justify-between mt-4">
-                                    <button class="btn"><i class="fa-solid fa-info cursor-pointer" data-word-id="${word.id}"></i></button>
+                                    <button class="btn information" data-word-id="${word.id}"><i class="fa-solid fa-info cursor-pointer" ></i></button>
                                     <button class="btn"><i class="fa-solid fa-volume-high"></i></button>
                                 </div>
                             `;
                             vocabCardsContainer.appendChild(card);
                         });
 
-                        
-                        const infoIcons = document.querySelectorAll('.fa-info');
+
+                        const infoIcons = document.querySelectorAll('.information');
                         console.log(infoIcons);
                         infoIcons.forEach(icon => {
                             icon.addEventListener('click', () => {
@@ -158,20 +170,20 @@ document.addEventListener('DOMContentLoaded', function () {
             .catch(error => console.error('Error fetching vocabulary:', error));
     }
 
-    
+
     function fetchWordDetails(wordId) {
         fetch(`https://openapi.programming-hero.com/api/word/${wordId}`)
             .then(response => response.json())
             .then(data => {
                 if (data.status) {
                     const wordDetails = data.data;
-                    
-                    document.getElementById('modalWord').textContent = wordDetails.word;
-                    document.getElementById('modalPronunciation').textContent = `${wordDetails?.meaning|| 'No Data'}`;
-                    document.getElementById('modalSentence').textContent = `${wordDetails?.sentence|| 'No Data'}`;
-                    document.getElementById('modalSynonyms').textContent = `${wordDetails?.synonyms.join(', ')|| 'No Data'}`;
 
-            
+                    document.getElementById('modalWord').textContent = wordDetails.word;
+                    document.getElementById('modalPronunciation').textContent = `${wordDetails?.meaning || 'অর্থ নেয়'}`;
+                    document.getElementById('modalSentence').textContent = `${wordDetails?.sentence || 'No Data'}`;
+                    document.getElementById('modalSynonyms').textContent = `${wordDetails?.synonyms.join(', ') || ' '}`;
+
+
                     document.getElementById('wordModal').showModal();
                 }
             })
